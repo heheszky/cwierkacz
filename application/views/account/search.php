ca01c0125207c0eee72401ea7700f1ae93fa0443
R4ndom <damianp96@gmail.com>
2015-12-07 18:02:01 +0100

    szmieszki

---
+++ application/views/account/search.php
@@ -0,0 +1,14 @@
+<div class="col-md-offset-4 col-lg-3 col-md-3">
+	<div class="panel panel-default szukaj-profilu">
+		<div class="panel-heading">Damian Plewa (damianp96@gmail.com) <a href='/sledz/<id>' class='sledz'>Śledź</a></div>
+	</div>
+	<div class="panel panel-default szukaj-profilu">
+		<div class="panel-heading">Damian Plewa (damianp96@gmail.com) <a href='/sledz/<id>' class='sledz'>Śledź</a></div>
+	</div>
+	<div class="panel panel-default szukaj-profilu">
+		<div class="panel-heading">Damian Plewa (damianp96@gmail.com) <a href='/sledz/<id>' class='sledz'>Śledź</a></div>
+	</div>
+	<div class="panel panel-default szukaj-profilu">
+		<div class="panel-heading">Damian Plewa (damianp96@gmail.com) <a href='/sledz/<id>' class='sledz'>Śledź</a></div>
+	</div>
+</div>
